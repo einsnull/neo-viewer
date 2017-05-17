4bc3c85ca979ef54aa7a0215d435a6180c0a3dae
tyuownu <tyuownu@gmail.com>
2017-05-17 13:16:40 +0800

    feat(zoom in/out when pause): realized the function zoom in/out when pause 1. zoom in/out when pause 2. change code style [   changes ] C: src/viewer.cpp, A: include/color.h [ reference ] [  computer ]

--- src/viewer.cpp
+++ src/viewer.cpp
@@ -10,94 +10,149 @@
 
 #include <neo/neo.hpp>
 
-#include <angle.h>
-#include <button.h>
 
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 
-// Zoom into 5x5 meter area
+#include <angle.h>
+#include <button.h>
+#include <color.h>
+
+// init distance show on windows
 const constexpr auto initDistance = 16 * 100.;
 
-// Use cream for the background and denim for points
-static const sf::Color kColorRed{255, 0, 0};
-static const sf::Color kColorCream{250, 240, 230};
-static const sf::Color kColorDenim{80, 102, 127};
-static const sf::Color kColorBlack{0, 0, 0};
-static const sf::Color kColorWhite{255, 255, 255};
-// color for background
-static const sf::Color kColorSlateBlue{106, 90, 205};
-static const sf::Color kColorMidnightBlue{25, 25, 112};
-static const sf::Color kColorDarkSlateBlue{72, 61, 139};
-static const sf::Color kColorBlue4{0, 0, 255};
-static const sf::Color kColorSlateBlue4{15, 16, 64};
 
 // windows zoom in/out frequency
 const sf::Time TimePreFrame = sf::seconds(1.f/20.f);
 
 
 // One circle per angle / distance measurement
-using PointCloud = std::vector<sf::CircleShape>;
+struct radian_distance_pointcloud {
+    neo::sample scan_data;
+    sf::CircleShape point;
+};
+using PointCloud = std::vector<struct radian_distance_pointcloud>;
 using PointCloudMutex = std::mutex;
 
+// build pointcloud when running
+void buildPointCloud(neo::scan *scan, AngleCircles *circles, PointCloudMutex &pointCloudMutex, PointCloud &pointCloud) {
+    auto currentDistance = circles->getDistance();
+    const auto windowSize = circles->windows_.getSize();
+    const auto windowMinSize = std::min(windowSize.x, windowSize.y);
+    const constexpr auto kDegreeToRadian = 0.017453292519943295;
+
+    PointCloud localPointCloud;
+
+    for (auto sample : scan->samples) {
+        const auto distance = static_cast<double>(sample.distance);
+
+        if (distance > currentDistance)
+            continue;
+        // unnecessary?
+
+        const auto degree = std::fmod((static_cast<double>(sample.angle) / 1000. + 90.), 360.);
+        const auto radian = degree * kDegreeToRadian;
+
+        auto x = std::cos(radian) * distance;
+        auto y = std::sin(radian) * distance;
+
+        x = x + currentDistance;
+        y = y + currentDistance;
+        x = (x / (2 * currentDistance)) * windowMinSize;
+        y = (y / (2 * currentDistance)) * windowMinSize;
+
+        sf::CircleShape point{2.0f, 20};
+        point.setPosition(x - 1, windowMinSize - y - 1);
+
+        point.setFillColor(sf::Color::White);
+        struct radian_distance_pointcloud temp = {sample, point};
+        localPointCloud.push_back(std::move(temp));
+    }
+
+    // display LiDAR position
+    sf::CircleShape point{4.0f, 8};
+    point.setPosition(windowSize.x / 2 - 4, windowSize.y / 2 - 4);
+    point.setFillColor(sf::Color::Red);
+    struct radian_distance_pointcloud temp = {{0, 0, 0}, point};
+    localPointCloud.push_back(std::move(temp));
+    {
+        // Now swap in the new point cloud
+        std::lock_guard<PointCloudMutex> sentry{pointCloudMutex};
+        pointCloud = std::move(localPointCloud);
+    }
+}
+
+// build pointcloud when pause, zoom in/out
+void buildPointCloudWhenPause(AngleCircles *circles, PointCloudMutex& pointCloudMutex, PointCloud& pointCloud) {
+    auto currentDistance = circles->getDistance();
+    const auto windowSize = circles->windows_.getSize();
+    const auto windowMinSize = std::min(windowSize.x, windowSize.y);
+    const constexpr auto kDegreeToRadian = 0.017453292519943295;
+
+    PointCloud localPointCloud;
+
+    for (auto sample : pointCloud) {
+        const auto distance = static_cast<double>(sample.scan_data.distance);
+        const auto degree = std::fmod((static_cast<double>(sample.scan_data.angle) / 1000. + 90.), 360.);
+        const auto radian = degree * kDegreeToRadian;
+
+        auto x = std::cos(radian) * distance;
+        auto y = std::sin(radian) * distance;
+
+        x = x + currentDistance;
+        y = y + currentDistance;
+        x = (x / (2 * currentDistance)) * windowMinSize;
+        y = (y / (2 * currentDistance)) * windowMinSize;
+
+        sf::CircleShape point{2.0f, 20};
+        point.setPosition(x - 1, windowMinSize - y - 1);
+
+        point.setFillColor(sf::Color::White);
+        struct radian_distance_pointcloud temp = {sample.scan_data, point};
+        localPointCloud.push_back(std::move(temp));
+    }
+
+    sf::CircleShape point{4.0f, 8};
+    point.setPosition(windowSize.x / 2 - 4, windowSize.y / 2 - 4);
+    point.setFillColor(sf::Color::Red);
+    struct radian_distance_pointcloud temp = {{0, 0, 0}, point};
+    localPointCloud.push_back(std::move(temp));
+    {
+        // Now swap in the new point cloud
+        std::lock_guard<PointCloudMutex> sentry{pointCloudMutex};
+        pointCloud = std::move(localPointCloud);
+    }
+}
 int main(int argc, char* argv[]) try {
 	if (argc != 2) {
 		std::cout << "Usage: ./neo_viewer /dev/ttyACM0\n";
 		return EXIT_FAILURE;
 	}
-
-	// sf::ContextSettings settings;
-	// settings.antialiasingLevel = 8;
-	// sf::RenderWindow window(sf::VideoMode(800, 800), "Micvision Neo LiDAR", sf::Style::Default, settings);
-
-	// window.setFramerateLimit(30);
-	// window.setActive(false); // activated on render thread
-
 	PointCloud pointCloud;
 	PointCloudMutex pointCloudMutex;
-	// sf::Texture mTexture;
-	// sf::Sprite mPlayer;
-	// mTexture.loadFromFile("../images/logo.jpg");
-	// mPlayer.setTexture(mTexture);
-	// mPlayer.setPosition(0.0f, 0.0f);
 
 	// Render thread displays the point cloud
 	AngleCircles circles(800, 16*100.);
 	AngleLines lines(800);
-    // neo::Buttons button;
 	const auto worker = [&](sf::RenderWindow* window) {
 		sf::Clock clock;
 		sf::Time time_since_last_update = sf::Time::Zero;
 		while (window->isOpen()) {
-			//sf::Event event;
-
-			//while (window->pollEvent(event)) {
-			//if (event.type == sf::Event::Closed)
-				//window->close();
-				//else if (event.type == sf::Event::KeyPressed)
-				//if (event.key.code == sf::Keyboard::Escape)
-					//window->close();
-			//}
 			sf::Time elapsed_time = clock.restart();
 			time_since_last_update += elapsed_time;
 
 			while (time_since_last_update > TimePreFrame) {
 				time_since_last_update -= TimePreFrame;
 				circles.processEvents();
-				//circles.processPress();
 				window->clear(kColorSlateBlue4);
 				{
 					std::lock_guard<PointCloudMutex> sentry{pointCloudMutex};
-
-					// window->draw(mPlayer);
 					circles.draw();
 					lines.draw(window);
-                    // button.draw(window);
 					for (auto point : pointCloud)
-						window->draw(point);
+						window->draw(point.point);
 				}
-
 				window->display();
 			}
 
@@ -110,13 +165,12 @@ int main(int argc, char* argv[]) try {
 
 	// Now start scanning in the second thread, swapping in new points for every scan
 	neo::neo device{argv[1]};
-	// device.set_motor_speed(5);
-	// device.start_scanning();
 
 	neo::scan scan;
     std::cout << "Device connect successful" << std::endl;
 
 	while (circles.windows_.isOpen()) {
+        // deal with press button situation
         if (circles.getButtonStatus() == neo::ButtonStatus::BUTTON_START) {
             static bool first_start = true;
             // std::cout << "start running" << std::endl;
@@ -142,70 +196,15 @@ int main(int argc, char* argv[]) try {
             continue;
         } else if (circles.getButtonStatus() == neo::ButtonStatus::BUTTON_NOT_RUN) {
             // std::cout << "Not_run" << std::endl;
+            buildPointCloudWhenPause(&circles, pointCloudMutex, pointCloud);
             continue;
         }
-
         // difference between BUTTON_NOT_RUN and BUTTON_NONE
         // BUTTON_NOT_RUN: the scanning stopped.
         // BUTTON_NONE: the scanner running and send data.
 
 		scan = device.get_scan();
-		auto currentDistance = circles.getDistance();
-        // std::cout << "currentDistance: " << currentDistance << std::endl;
-
-		// 40m max radius => display as 80m x 80m square
-		const auto windowSize = circles.windows_.getSize();
-		const auto windowMinSize = std::min(windowSize.x, windowSize.y);
-
-		PointCloud localPointCloud;
-
-		for (auto sample : scan.samples) {
-			const constexpr auto kDegreeToRadian = 0.017453292519943295;
-
-			const auto distance = static_cast<double>(sample.distance);
-
-			// Discard samples above our we zoomed-in view box
-			if (distance > currentDistance)
-				continue;
-
-			// From milli degree to degree and adjust to device orientation
-			const auto degree = std::fmod((static_cast<double>(sample.angle) / 1000. + 90.), 360.);
-			const auto radian = degree * kDegreeToRadian;
-
-			// From angle / distance to to Cartesian
-			auto x = std::cos(radian) * distance;
-			auto y = std::sin(radian) * distance;
-
-			// Make positive
-			x = x + currentDistance;
-			y = y + currentDistance;
-
-			// Scale to window size
-			x = (x / (2 * currentDistance)) * windowMinSize;
-			y = (y / (2 * currentDistance)) * windowMinSize;
-
-			sf::CircleShape point{2.0f, 20};
-			point.setPosition(x-1, windowMinSize - y-1);
-
-			// Base transparency on signal strength
-			auto color = kColorRed;
-			color.a = sample.signal_strength;
-			point.setFillColor(sf::Color::White);
-
-			localPointCloud.push_back(std::move(point));
-		}
-
-		// display LiDAR position
-		sf::CircleShape point{4.0f, 8};
-		point.setPosition(windowSize.x / 2-4, windowSize.y / 2-4);
-		point.setFillColor(sf::Color::Red);
-		localPointCloud.push_back(std::move(point));
-
-		{
-			// Now swap in the new point cloud
-			std::lock_guard<PointCloudMutex> sentry{pointCloudMutex};
-			pointCloud = std::move(localPointCloud);
-		}
+        buildPointCloud(&scan, &circles, pointCloudMutex, pointCloud);
 	}
 
 	device.stop_scanning();
